74b07e3c42aff74d7893b9610c46c884233ead77
Koushik Jayanthi <30872050+Koushik999999@users.noreply.github.com>
2018-10-30 19:13:08 +0530

    Detecting the puns is completed

--- Task 1/Task1.py
+++ Task 1/Task1.py
@@ -59,3 +59,100 @@ for s in our_answer:
 Y = list(our_answer.values())
 # print(X[4]);
 # print(Y[4]);
+X = np.array(X)
+
+class_mean = {}
+class_var = {}
+class_ind ={}
+labels = []
+cnt = 0
+for i in range(len(X)):
+    clas = str(Y[i])
+    if clas not in class_ind.keys():
+        class_mean[clas]=[]
+        class_var[clas]=[]
+        class_ind[clas]=[]
+        labels.append(clas)
+    class_ind[clas].append(cnt)
+    cnt+=1
+
+images = np.array(X)
+
+images = np.array(images)
+
+image_mean =images - np.mean(images,axis=0)
+# print(len(images))
+
+
+
+cov_image = np.dot(image_mean.T,image_mean)
+image_lam, image_vec = np.linalg.eig(cov_image)
+
+# print(image_vec)
+
+indi = np.argsort(-image_lam)
+
+
+image_vec = image_vec[:,indi]
+
+# sorted_vec = image_vec[:,np.flip(indi,axis=0)]
+
+
+N = 10
+
+n_vec = image_vec[:,0:N]
+n_data = np.matmul(n_vec.T,images.T)
+n_data = n_data.T
+
+for i in labels:
+    dattt = np.array(n_data[class_ind[i],:])
+    class_mean[i].append(np.mean(dattt,axis=0))
+    class_var[i].append(np.var(dattt,axis=0))
+
+fp1 = open('./test_final1.txt')
+
+test = fp1.readlines()
+
+flag = 0
+
+for s in test:
+    avg = [0]*25
+    s = s.split(' ')
+    for j in range(len(s)):
+        for k in range(len(avg)):
+            avg[k] +=model[s[j]][k]
+    for j in range(len(avg)):
+        avg[j] /= len(s)
+
+    test_image = avg
+    array = np.array(test_image)
+    array = array.flatten()
+
+
+    test_data = np.matmul(n_vec.T,array.T)
+    test_data = test_data.T
+    max_p = 0
+    ans = ''
+    for clas in labels:
+        prob  = 1
+        mean = class_mean[clas][0]
+        var  = class_var[clas][0]
+
+
+        for j in range(len(test_data)):
+            val = 1/np.sqrt(2*np.pi*var[j])
+            prob = prob*(np.exp(-abs(np.square(test_data[j]-mean[j])/var[j])))*val
+        # prob = prob * class_count[clas]
+        if prob > max_p:
+            max_p = prob
+            ans = clas
+
+    if ans == '1':
+        flag =1
+        break
+
+if flag == 1:
+    print('YES; the given context contains a pun')
+else:
+    print('NO; the given context does not contain a pun')
+
